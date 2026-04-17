99bab482ca1c19a2b0c8f79eaa64cec8e5bd3579
Beebarh.jy <barbex.jy@gmail.com>
2026-04-17 22:08:16 +0100

    changed db

--- db.php
+++ db.php
@@ -3,10 +3,11 @@ $host = getenv('DB_HOST') ?: 'profile_db';
 $db = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$port = getenv('DB_PORT') ?: '16169';
+$port = getenv('DB_PORT') ?: '3306';
 
 try {
     // We explicitly include the port 3306 for the internal network
+
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
