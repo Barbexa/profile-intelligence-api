554f60a36dcda987955d8fa1dfed79931e213c6b
Beebarh.jy <barbex.jy@gmail.com>
2026-04-17 22:46:04 +0100

    change

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST');
+$host = getenv('DB_HOST') ?: 'profile_db'; // Default to localhost if DB_HOST is not set
 $db = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$port = getenv('DB_PORT');
+$port = getenv('DB_PORT') ?: 3306; // Default to 3306 if DB_PORT is not set
 
 try {
     // We explicitly include the port 3306 for the internal network
