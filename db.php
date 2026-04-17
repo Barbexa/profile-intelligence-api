bbaf24a03649f9f42b6348bac185d4b2ab3256f0
Beebarh.jy <barbex.jy@gmail.com>
2026-04-17 12:23:07 +0100

    changed db to fit pxxl hosting

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
+
 <?php
-$host = "localhost";
-$db = "profile_db";
-$user = "root";
-$pass = "";
+// Use getenv() to pull from PXXXL settings
+$host = getenv('DB_HOST');
+$db = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo json_encode([
-        "status" => "error",
-        "message" => "Database connection failed"
-    ]);
+    header('Content-Type: application/json', true, 500);
+    echo json_encode(["status" => "error", "message" => "Database Connection Failed"]);
     exit;
-}
+} ?>
